67806866ee771fa53ec7194265eb70f9f37a25a5
Théo LUDWIG <contact@theoludwig.fr>
2023-07-02 12:18:21 +0200

    refactor: usage of node:test instead of tap

--- src/__test__/HTMLValidatorCommand.test.ts
+++ src/__test__/HTMLValidatorCommand.test.ts
@@ -1,21 +1,23 @@
-import tap from 'tap'
+import test from 'node:test'
+import assert from 'node:assert/strict'
+
 import { execa } from 'execa'
 
 import { cli } from '../cli.js'
 import { HTMLValidatorCommand } from '../HTMLValidatorCommand.js'
 
-await tap.test('html-w3c-validator', async (t) => {
-  await t.test('should be instance of the command', async (t) => {
+await test('html-w3c-validator', async (t) => {
+  await t.test('should be instance of the command', async () => {
     const command = cli.process([])
-    t.equal(command instanceof HTMLValidatorCommand, true)
+    assert(command instanceof HTMLValidatorCommand)
   })
 
-  await t.test('succeeds and validate the html correctly', async (t) => {
+  await t.test('succeeds and validate the html correctly', async () => {
     const exampleURL = new URL('../../example', import.meta.url)
     process.chdir(exampleURL.pathname)
     await execa('rimraf', ['node_modules'])
     await execa('npm', ['install'])
     const { exitCode } = await execa('npm', ['run', 'test:html-w3c-validator'])
-    t.equal(exitCode, 0)
+    assert.strictEqual(exitCode, 0)
   })
 })
